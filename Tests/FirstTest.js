9ceb6ee08f9114416881331f47ad1946e5b175f6
htmanh <htmanh@avaya.com>
2019-05-07 15:52:17 +0700

    add protractor-jasmine2-html-report

--- Tests/FirstTest.js
+++ Tests/FirstTest.js
@@ -4,5 +4,10 @@ describe("Test entering into an input box", function () {
     it("executing input box test", function () {
         browser.get("http://juliemr.github.io/protractor-demo/");
         element(by.model("first")).sendKeys("5");
-    })
+        element(by.binding("latest")).getText().then(
+            text => {
+                console.log(text);
+            }
+        );
+    });
 });
--- conf.js
+++ conf.js
@@ -1,12 +1,18 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['Tests/spec.js'],
+    specs: ['Tests/handle-dropdown.js'],
     framework: 'jasmine2',
     capabilities: {
         'browserName': 'chrome'
     },
     onPrepare: function () {
-        var AllureReporter = require('jasmine-allure-reporter');
+        const Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter')
+        jasmine.getEnv().addReporter(new Jasmine2HtmlReporter({
+            savePath: './reports/',
+            screenshotsFolder: 'images'
+        }));
+        
+        const AllureReporter = require('jasmine-allure-reporter');
         jasmine.getEnv().addReporter(new AllureReporter());
         jasmine.getEnv().afterEach(function(done){
             browser.takeScreenshot().then(function (png) {
@@ -16,5 +22,6 @@ exports.config = {
                 done();
             })
         });
+
     }
 };
