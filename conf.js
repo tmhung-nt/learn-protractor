73a0882d89cb21b4178a27224579dc80ab5ee2b0
htmanh <htmanh@avaya.com>
2019-05-07 11:03:03 +0700

    adding launch.json to run test in Visual Studio Code

--- conf.js
+++ conf.js
@@ -1,6 +1,6 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['Tests/FirstTest.js'],
+    specs: ['Tests/spec.js'],
     framework: 'jasmine2',
     capabilities: {
         'browserName': 'chrome'
