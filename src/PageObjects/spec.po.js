48c0fffd1bd3adf48e010aecce605c60134ec525
htmanh <htmanh@avaya.com>
2019-05-22 09:43:42 +0700

    add page object example

---
+++ src/PageObjects/spec.po.js
@@ -0,0 +1,28 @@
+let firstNumberEle,
+    secondNumberEle,
+    operationEle,
+    goBtnEle,
+    calculationResultEle;
+
+module.exports = {
+  init: () => {
+    firstNumberEle = element(by.model('first'));
+    secondNumberEle = element(by.model('second'));
+    goBtnEle = element(by.buttonText('Go!'));
+    calculationResultEle = element(by.binding('latest'));
+
+  },
+  get: (string) => {
+    console.log(browser.getSession().value_.id_);
+    return browser.get(string);
+  },
+  setValueFirstNumber: (value) => {
+    return firstNumberEle.sendKeys(value);
+  },
+  setValueSecondNumber: (value) => {
+    return secondNumberEle.sendKeys(value);
+  },
+  getCalculationResult: () => {
+    return calculationResultEle.getText();
+  }
+}
