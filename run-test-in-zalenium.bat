9d6ae3ba2e690e5730a387a0888325f8b749f467
htmanh <htmanh@avaya.com>
2019-05-23 09:38:17 +0700

    update run-test-in-zalenium.bat

--- run-test-in-zalenium.bat
+++ run-test-in-zalenium.bat
@@ -5,6 +5,9 @@ cls
 echo "Pull latest elgalu/selenium image"
 call docker pull elgalu/selenium
 
+echo "Remove protractor-demo_e2e image to force rebuild"
+call docker rmi protractor-demo_e2e
+
 echo "start zalenium hub"
 call docker-compose -f .\zalenium-compose.yml up -d hub
 
